e601a05e6e96a70438e4a5d2a1f9d387141dbf6e
blackm-97 <blackm97@msu.edu>
2025-02-26 01:07:51 +0000

    Fixed

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
+  eleventyConfig.setServerPassthroughCopyBehavior("copy");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("javascript");
